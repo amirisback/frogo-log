d14b3e2ee4d264b2f11b7f25bfecfefeb4e470c1
amirisback <faisalamircs@gmail.com>
2021-01-14 14:51:12 +0700

    update: build.gradle

--- app/src/main/java/com/frogobox/logcat/MainActivity.kt
+++ app/src/main/java/com/frogobox/logcat/MainActivity.kt
@@ -2,10 +2,18 @@ package com.frogobox.logcat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.frogobox.logcat.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
+        setContentView(binding.root)
+
     }
+
 }
