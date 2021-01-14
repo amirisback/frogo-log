35b50e754f5999077e2ff9c6457a4d2152cba2e7
amirisback <faisalamircs@gmail.com>
2021-01-14 14:43:50 +0700

    add: FrogoLog Lib

---
+++ frogolog/src/main/java/com/frogobox/frogolog/FrogoLog.kt
@@ -0,0 +1,16 @@
+package com.frogobox.frogolog
+
+/*
+ * Created by Faisal Amir on 14/01/2021
+ * LogCat Source Code
+ * -----------------------------------------
+ * Name     : Muhammad Faisal Amir
+ * E-mail   : faisalamircs@gmail.com
+ * Github   : github.com/amirisback
+ * -----------------------------------------
+ * Copyright (C) 2021 FrogoBox Inc.      
+ * All rights reserved
+ *
+ */
+class FrogoLog {
+}
