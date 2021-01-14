623f0b0340955ebcbd1e0dfefe162f8610aee643
amirisback <faisalamircs@gmail.com>
2021-01-14 15:50:54 +0700

    add: Simple Sample

--- app/src/main/java/com/frogobox/logcat/MainActivity.kt
+++ app/src/main/java/com/frogobox/logcat/MainActivity.kt
@@ -1,18 +1,29 @@
 package com.frogobox.logcat
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import androidx.appcompat.app.AppCompatActivity
+import com.frogobox.frogolog.FrogoLog
 import com.frogobox.logcat.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
         setContentView(binding.root)
+
+        binding.apply {
+
+            btnLog.setOnClickListener {
+
+                FrogoLog.d()
+                FrogoLog.d("Debug")
+
+            }
+
+        }
 
     }
 
